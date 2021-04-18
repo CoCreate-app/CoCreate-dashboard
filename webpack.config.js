0b11282f167f8a84f1cdcd76772d7452d766c697
Frank Pagan <frank@cocreate.app>
2021-04-18 01:44:20 +0000

    update all workflows

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,85 @@
-// Webpack uses this to work with directories
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path")
+const TerserPlugin = require("terser-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+let isProduction = process.env.NODE_ENV === "production"
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
-let isProduction = process.env.NODE_ENV === 'production';
-
-// This is main configuration object.
-// Here you write different options and tell Webpack what to do
 module.exports = {
-
-  // Path to your entry point. From this file Webpack will begin his work
   entry: {
-    'CoCreate-dashboard': './src/CoCreate-dashboard.js',
+    "CoCreate-dashboard": "./src/CoCreate-dashboard.js",
   },
-
- // Path and filename of your result bundle.
-  // Webpack will bundle all JavaScript into this file
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: isProduction ? '[name].min.js' : '[name].js',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    library: ['CoCreate', 'dashboard'],
+    path: path.resolve(__dirname, "dist"),
+    filename: isProduction ? "[name].min.js" : "[name].js",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    library: ["CoCreate", "dashboard"],
     globalObject: "this",
+    // publicPath: 'https://server.cocreate.app/CoCreateJS/dist/'
   },
 
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
   // on final bundle. For now we don't need production's JavaScript
   // minifying and other thing so let's set mode to development
-  mode: isProduction ? 'production' : 'development',
+  mode: isProduction ? "production" : "development",
   module: {
-    rules: [{
-        test: /\.js$/,
+    rules: [
+      {
+        test: /.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            plugins: ["@babel/plugin-transform-modules-commonjs"],
+          },
+        },
       },
-    ]
+      {
+        test: /.css$/i,
+        use: [
+          { loader: "style-loader", options: { injectType: "linkTag" } },
+          "file-loader",
+        ],
+      },
+    ],
   },
 
   // add source map
-  ...(isProduction ? {} : { devtool: 'eval-source-map' }),
+  ...(isProduction ? {} : { devtool: "eval-source-map" }),
 
-  // add uglifyJs
   optimization: {
-    minimizer: [new UglifyJsPlugin({
-      uglifyOptions: {
-        // get options: https://github.com/mishoo/UglifyJS
-        drop_console: isProduction
-      },
-    })],
-  },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: true,
+        // cache: true,
+        parallel: true,
+        // sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+          // extractComments: 'all',
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+    splitChunks: {
+      chunks: "all",
+      minSize: 200,
+      // maxSize: 99999,
+      //minChunks: 1,
 
-};
+      cacheGroups: {
+        defaultVendors: false,
+      },
+    },
+  },
+}
