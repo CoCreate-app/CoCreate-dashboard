e461128c8c9941341d9a1e6bcdd17e40f4ca5b03
frankpagan <frank@cocreate.app>
2023-05-01 07:15:06 -0600

    parentDirectory

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -17,9 +17,10 @@ module.exports = {
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/dashboard",
-                "content-type": "text/html",
+                "parentDirectory": "{{parentDirectory}}",
+                "content-type": "{{content-type}}",
                 "public": "true",
-                "website_id": "5ffbceb7f11d2d00103c4535"
+                "website_id": "644d4bff8036fb9d1d1fd69c"
             }
         }
     ]
