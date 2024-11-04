7b28535ca495091e3015b6372a93d75c75bdf102
frankpagan <frank@cocreate.app>
2024-11-04 01:09:59 -0500

    feat: add prettier.config.js and format files

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    tabWidth: 4,
+    semi: true,
+    trailingComma: "none",
+    bracketSameLine: true,
+    useTabs: true,
+    overrides: [
+      {
+        files: ["*.json", "*.yml", "*.yaml"],
+        options: {
+          tabWidth: 2,
+        },
+      }
+    ],
+  };
